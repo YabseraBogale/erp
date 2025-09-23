2c98b66a7e2fd181e907ec4131f972e0124c918a
YabseraBogale <yabserapython@gmail.com>
2025-09-23 19:37:12 +0300

    first frontend and feature that will the module

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/YabseraBogale/erp/actors"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = db.AutoMigrate(&actors.EmployeeContact{}, &actors.Employee{},
+		&actors.Item{}, &actors.TransactionType{},
+		&actors.ItemLog{}, &actors.Checkout{})
+	if err != nil {
+		log.Println(err)
+	}
 }
